f991459f86e2452d0fb482ae0af2dfd8a2a2f62b
Alexandre <= alexandrec@alu.ufc.br>
2026-01-07 07:39:01 -0300

    test: introdução as validações de negócio

--- domain/src/test/java/com/fullcycle/admim/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admim/catalogo/domain/category/CategoryTest.java
@@ -31,4 +31,29 @@ class CategoryTest {
 
     }
 
+    @Test
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError(){
+
+        final String expectedName = null;
+        final var expectedDescription = "some description";
+        final var expectedErrorMessage = "'name' should be not null";
+        final var expectedErrorCount = 1;
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        final var err =  Assertions.assertThrows(
+                DomainException.class, () -> actualCategory.validate());
+
+
+        Assertions.assertEquals(expectedErrorMessage,actualCategory.getErrors().get(0).getMessage());
+        Assertions.assertEquals(expectedErrorCount,actualCategory.getErrors().size());
+
+    }
+
+
 }
