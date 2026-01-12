ad66a9f86f61e481ece873c7dc32bd36b486c6d6
Alexandre <= alexandrec@alu.ufc.br>
2026-01-12 17:12:27 -0300

    feat: implementando o caso de uso de criação de categoria

---
+++ application/src/main/java/com/fullcycle/admim/catalogo/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.fullcycle.admim.catalogo.application.category.create;
+
+import com.fullcycle.admim.catalogo.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+}
