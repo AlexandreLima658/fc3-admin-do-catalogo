55e5b5aa74fb9be03c9add0a24bff1de3385acff
Alexandre <= alexandrec@alu.ufc.br>
2026-01-27 10:55:52 -0300

    feat: adicionando o springboot na camada de infra

---
+++ infra/src/main/java/com/fullcycle/admim/catalogo/infra/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.fullcycle.admim.catalogo.infra.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcycle.admim.catalogo")
+public class WebServerConfig {
+}
