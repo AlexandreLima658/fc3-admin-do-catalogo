449c10fd132fb0e0ef99e43b39981a6a7c0e8f0b
Alexandre <= alexandrec@alu.ufc.br>
2026-01-30 15:10:16 -0300

    test: teste main

--- infra/src/test/java/com/fullcycle/admim/catalogo/infra/MainTest.java
+++ infra/src/test/java/com/fullcycle/admim/catalogo/infra/MainTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
-
     @Test
     public void testMain(){
         Assertions.assertNotNull(new Main());
