6a08da1cf61af64ba54b7e3d163062bebcd74e98
Alexandre <= alexandrec@alu.ufc.br>
2026-02-06 15:16:32 -0300

    feat: criando a implementação do gateway de categoria

---
+++ infra/src/main/java/com/fullcycle/admim/catalogo/infra/category/CategoryMySQLGateway.java
@@ -0,0 +1,46 @@
+package com.fullcycle.admim.catalogo.infra.category;
+
+import com.fullcycle.admim.catalogo.domain.category.Category;
+import com.fullcycle.admim.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admim.catalogo.domain.category.CategoryID;
+import com.fullcycle.admim.catalogo.domain.category.CategorySearchQuery;
+import com.fullcycle.admim.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admim.catalogo.infra.category.persistence.CategoryJpaRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class CategoryMySQLGateway implements CategoryGateway {
+
+    private final CategoryJpaRepository repository;
+
+    public CategoryMySQLGateway(final CategoryJpaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(CategoryID anId) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+}
