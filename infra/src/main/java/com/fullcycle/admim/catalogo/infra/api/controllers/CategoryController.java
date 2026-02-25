97c3cf6d88ce115de894f212b3012d40524f64c9
Alexandre <alexandrec@alu.ufc.br>
2026-02-25 19:45:21 -0300

    teste: craindo a estrategia para testes integrados de controller

--- infra/src/main/java/com/fullcycle/admim/catalogo/infra/api/controllers/CategoryController.java
+++ infra/src/main/java/com/fullcycle/admim/catalogo/infra/api/controllers/CategoryController.java
@@ -1,13 +1,22 @@
 package com.fullcycle.admim.catalogo.infra.api.controllers;
 
+import com.fullcycle.admim.catalogo.application.category.create.CreateCategoryUseCase;
 import com.fullcycle.admim.catalogo.domain.pagination.Pagination;
 import com.fullcycle.admim.catalogo.infra.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 
 @RestController
 public class CategoryController implements CategoryAPI {
+
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(final CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    }
 
     @Override
     public ResponseEntity<?> createCategory() {
